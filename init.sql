7ca39c6384b18399f727fa6901a6445776faa261
Tan Kongpithaksilp <45220855+tangentino@users.noreply.github.com>
2021-07-06 17:41:38 +0700

    Fixed indents

--- init.sql
+++ init.sql
@@ -10,48 +10,48 @@ CREATE TABLE restaurants (
 );
 
 CREATE TABLE menus (
-    menu_id serial PRIMARY KEY,
-    restaurant_id serial,
-    menu_type_id serial,
-    name TEXT UNIQUE NOT NULL,
-    list_pricing INT NOT NULL,
-  	FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
-  	FOREIGN KEY (menu_type_id) REFERENCES menu_types(menu_type_id)
+	menu_id serial PRIMARY KEY,
+	restaurant_id serial,
+	menu_type_id serial,
+	name TEXT UNIQUE NOT NULL,
+	list_pricing INT NOT NULL,
+	FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
+	FOREIGN KEY (menu_type_id) REFERENCES menu_types(menu_type_id)
 );
 
 CREATE TABLE menu_types (
-    menu_type_id serial PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL
+	menu_type_id serial PRIMARY KEY,
+	name TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE user_profiles (
-    user_profile_id serial PRIMARY KEY,
-    user_id serial,
-    firstname TEXT NOT NULL,
-    lastname TEXT NOT NULL,
-    phone_contact VARCHAR ( 10 ) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+	user_profile_id serial PRIMARY KEY,
+	user_id serial,
+	firstname TEXT NOT NULL,
+	lastname TEXT NOT NULL,
+	phone_contact VARCHAR ( 10 ) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE users (
-    user_id serial PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+	user_id serial PRIMARY KEY,
+	username TEXT UNIQUE NOT NULL,
+	password TEXT NOT NULL
 );
 
 CREATE TABLE reviews (
-    review_id serial PRIMARY KEY,
-    user_profile_id serial,
-    restaurant_id serial,
-    rating_id serial,
-    description TEXT,
-    created_on TIMESTAMP NOT NULL,
-    FOREIGN KEY (user_profile_id) REFERENCES user_profiles(user_profile_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
-    FOREIGN KEY (rating_id) REFERENCES ratings(rating_id)
+	review_id serial PRIMARY KEY,
+	user_profile_id serial,
+	restaurant_id serial,
+	rating_id serial,
+	description TEXT,
+	created_on TIMESTAMP NOT NULL,
+	FOREIGN KEY (user_profile_id) REFERENCES user_profiles(user_profile_id),
+	FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
+	FOREIGN KEY (rating_id) REFERENCES ratings(rating_id)
 );
 
 CREATE TABLE ratings (
-    rating_id serial PRIMARY KEY,
-    score INT NOT NULL
+	rating_id serial PRIMARY KEY,
+	score INT NOT NULL
 );
