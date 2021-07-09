83d92991e534db21963f5031ab46dc8396c66ae7
TaeXtream <59652919+TaeXtream@users.noreply.github.com>
2021-07-09 17:28:04 +0700

    Add adding review function

--- src/services/db.py
+++ src/services/db.py
@@ -52,6 +52,7 @@ class Database:
         return f"{cursor.rowcount} rows affected."
 
     def insert_row(self, statement: str, values=None):
+        """Run a SQL query to add row to the table and return row id."""
         cursor = self.execute_statement(statement, values)
         add_id = cursor.fetchone()[0]
         self.connection.commit()
