c435563cc0427110a6efeec804bcca8a432a352d
TaeXtream <59652919+TaeXtream@users.noreply.github.com>
2021-07-09 17:27:23 +0700

    Add First Version of account delete

--- src/services/security_api.py
+++ src/services/security_api.py
@@ -16,10 +16,15 @@ def get_username_by_id(user_id):
 
 def add_new_account_api(profile: User_Profile):
     user_id = add_new_user_api(profile.username, profile.password)
-    if ((user_id is not None) and
-            (add_new_user_profile_api(user_id, profile.firstname, profile.lastname, profile.phone_number) is not None)):
-        print("Successfully register account {}".format(profile.username))
-        return True
+    if user_id is not None:
+        profile_id = add_new_user_profile_api(user_id, profile.firstname, profile.lastname, profile.phone_number)
+        if profile_id is not None:
+            print("Successfully register account {}".format(profile.username))
+            return True
+        else:
+            print("Fail to register profile of {}".format(profile.firstname))
+            delete_user_api(user_id)
+            return False
     print("Fail to register account {}".format(profile.username))
     return False
 
@@ -37,3 +42,14 @@ def add_new_user_profile_api(user_id: int, firstname: str, lastname: str, phone_
     return database.insert_row(
         "INSERT INTO user_profiles(user_id, firstname ,lastname, phone_contact) VALUES (%d, %s, %s, %s)",
         (user_id, firstname, lastname, phone_number,))
+
+
+def delete_user_api(user_id):
+    return database.delete_row("DELETE FROM users u where u.user_id=%s", (user_id,))
+
+
+def delete_account_api(user_id):
+    database.delete_row("DELETE FROM user_profiles p where p.user_id=%s", (user_id,))
+    database.delete_row("DELETE FROM users u where u.user_id=%s", (user_id,))
+    # TODO: add feature to check if successfully delete or not
+    return
