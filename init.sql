bfd22dcb0b00677558b84caabb1d198cd155ff46
nakarinh14 <nakarinh14@gmail.com>
2021-07-14 17:00:32 +0700

    Update syntax in init.sql

--- init.sql
+++ init.sql
@@ -1,12 +1,16 @@
 -- Sample, add more table stuff in here.
 
-CREATE DATABASE food_review_db;
-
-\connect food_review_db
+CREATE TABLE franchises
+(
+    franchise_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL
+);
 
 CREATE TABLE restaurants
 (
-    restaurant_id serial PRIMARY KEY,
+    restaurant_id SERIAL PRIMARY KEY,
+    franchise_id INT REFERENCES franchises (franchise_id),
     name          TEXT UNIQUE NOT NULL,
     phone_contact VARCHAR(10) NOT NULL,
     address       TEXT UNIQUE NOT NULL,
@@ -16,69 +20,63 @@ CREATE TABLE restaurants
 
 CREATE TABLE menu_types
 (
-    menu_type_id serial PRIMARY KEY,
+    menu_type_id SERIAL PRIMARY KEY,
     name         TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE menus
 (
-    menu_id       serial PRIMARY KEY,
-    restaurant_id serial,
-    menu_type_id  serial,
-    name          TEXT UNIQUE NOT NULL,
-    list_pricing  INT         NOT NULL,
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id),
-    FOREIGN KEY (menu_type_id) REFERENCES menu_types (menu_type_id)
+    menu_id       SERIAL PRIMARY KEY,
+    restaurant_id INT REFERENCES restaurants (restaurant_id) NOT NULL ,
+    menu_type_id  INT REFERENCES menu_types (menu_type_id) NOT NULL,
+    name          TEXT NOT NULL,
+    list_pricing  INT NOT NULL,
+    UNIQUE(name, menu_type_id, restaurant_id)
 );
 
 CREATE TABLE users
 (
-    user_id  serial PRIMARY KEY,
+    user_id  SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT        NOT NULL
 );
 
 CREATE TABLE user_profiles
 (
-    user_profile_id serial PRIMARY KEY,
-    user_id         serial,
+    user_profile_id SERIAL PRIMARY KEY,
+    user_id         INT REFERENCES users (user_id) ON DELETE CASCADE NOT NULL,
     firstname       TEXT        NOT NULL,
     lastname        TEXT        NOT NULL,
-    phone_contact   VARCHAR(10) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+    phone_contact   VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE ratings
 (
-    rating_id serial PRIMARY KEY,
+    rating_id SERIAL PRIMARY KEY,
     score     INT NOT NULL
 );
 
 CREATE TABLE reviews
 (
-    review_id       serial PRIMARY KEY,
-    user_profile_id serial,
-    restaurant_id   serial,
-    rating_id       serial,
+    review_id       SERIAL PRIMARY KEY,
+    user_profile_id INT REFERENCES user_profiles (user_profile_id) NOT NULL, 
+    restaurant_id   INT REFERENCES restaurants (restaurant_id) NOT NULL,
+    rating_id       INT REFERENCES ratings (rating_id)  NOT NULL,
     description     TEXT,
-    created_on      TIMESTAMP NOT NULL,
-    FOREIGN KEY (user_profile_id) REFERENCES user_profiles (user_profile_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id),
-    FOREIGN KEY (rating_id) REFERENCES ratings (rating_id)
-);
-
-CREATE TABLE restaurant_tags (
-    restaurant_tag_id serial PRIMARY KEY,
-    tag_id serial,
-    restaurant_id serial,
-    FOREIGN KEY (tag_id) REFERENCES tags(tag_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+    created_on      TIMESTAMP NOT NULL
 );
 
 CREATE TABLE tags (
-    tag_id serial PRIMARY KEY,
-    description TEXT
+    tag_id SERIAL PRIMARY KEY,
+    description TEXT UNIQUE NOT NULL
 );
+
+CREATE TABLE restaurant_tags (
+    restaurant_tag_id SERIAL PRIMARY KEY,
+    tag_id INT REFERENCES tags(tag_id) NOT NULL,
+    restaurant_id INT REFERENCES restaurants(restaurant_id) NOT NULL
+);
+
 
 INSERT INTO restaurants(name, phone_contact, address, created_on, is_open)
 VALUES ('Jojo Pizzeria', 7124707705, '4925 Pin Oak Drive Benton Iowa', '2016-06-22'::timestamp, true),
